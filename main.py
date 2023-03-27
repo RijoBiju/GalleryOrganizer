0f78c1c492f0419e734f93db93048838806aaf41
Rijo Biju <rijobijua.github@outlook.com>
2023-03-27 20:16:41 +0530

    mime types

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import sys
 from src import mediaorganizer, foldermanager
-import pathlib
+from pathlib import Path
 import os
+import mimetypes
 
-image_extensions = ['.JPG', '.JPEG', '.HEIC']
-video_extensions = ['.MP4', '.MOV']
+image_mime_types = ['image/jpeg', 'image/heic']
+video_mime_types = ['video/mp4', 'video/quicktime']
 
 
 if __name__ == "__main__":
     folder_manager = foldermanager.FolderManager()
     working_directory = sys.argv[1]
     files = os.listdir(working_directory)
-    for i in files:
-        file_location = f'{working_directory}\\{files[i]}'
-        file_extension = pathlib.Path(file_location).suffix
-        if file_extension in image_extensions:
-            image_parser = mediaorganizer.MediaOrganizer(folder_manager)
-            image_parser.image_path = file_location
-            exifdata = image_parser.get_image_metadata()
-            image_parser.process_image_exif(exifdata)
-            print('image')
-        elif file_extension in video_extensions:
-            print('video')
+    media_organizer = mediaorganizer.MediaOrganizer(folder_manager)
+    for file_name in files:
+        file_location = Path(working_directory, file_name)
+        mime_type, _ = mimetypes.guess_type(file_location)
+        if mime_type in image_mime_types:
+            media_organizer.image_path = file_location
+            exifdata = media_organizer.get_image_metadata()
+            media_organizer.process_image_exif(exifdata)
+        elif mime_type in video_mime_types:
+            pass
